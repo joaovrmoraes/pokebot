892de99340ae77d0473835f226fffbc6cf777a4b
joaovrmoraes <joao.v.mpessoal@gmail.com>
2024-03-27 21:18:07 -0300

    :construction: wip:  generating a random pokemon and add reaction

--- src/tasks/spawn-pokemon.ts
+++ src/tasks/spawn-pokemon.ts
@@ -1,18 +1,17 @@
 import { getPokemon } from "@/commands/get-pokemon";
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Channel } from "discord.js";
-import { pokemonEmbed} from "@/components/pokemon-embed"
+import { pokemonEmbed } from "@/components/pokemon-embed"
 
 export async function spanwPokemon({ channel }: { channel: Channel }) {
   const randomNumber = Math.floor(Math.random() * 151) + 1;
 
   const pokemonData = await getPokemon(randomNumber);
-  
-  const buttonConfirm = new ButtonBuilder().setCustomId('confirm').setLabel('🔴 GO POKEBALL').setStyle(ButtonStyle.Success);
 
-  const row = new ActionRowBuilder().addComponents(buttonConfirm);
+  if (channel.isTextBased()) {
+    const message = await channel.send({ components: [], embeds: [pokemonEmbed(pokemonData)], stickers: [] });
+    message.react('🔴');
 
-  if (channel.isTextBased()) { 
-    const message = await channel.send({components: [row], embeds: [ pokemonEmbed(pokemonData)]})
+    message.awaitReactions({ filter: (reaction, user) => reaction.emoji.name === '🔴' && user.id !== message.author.id, max: 1, time: 30000, errors: ['time'] })
+
   }
-  
 }
